18a7723b27390892c1834557b5b41ad41e10ad4a
Administrator <1270478481@qq.com>
2018-09-05 10:11:49 +0800

    the add user module

--- transport-user/src/main/java/com/yoo/user/TransportUserApplication.java
+++ transport-user/src/main/java/com/yoo/user/TransportUserApplication.java
@@ -1,12 +1,12 @@
-package com.yoo.transport;
+package com.yoo.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TransportApplication {
+public class TransportUserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TransportApplication.class, args);
+		SpringApplication.run(TransportUserApplication.class, args);
 	}
 }
--- transport-user/src/test/java/com/yoo/user/TransportUserApplicationTests.java
+++ transport-user/src/test/java/com/yoo/user/TransportUserApplicationTests.java
@@ -1,4 +1,4 @@
-package com.yoo.transport;
+package com.yoo.user;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TransportApplicationTests {
+public class TransportUserApplicationTests {
 
 	@Test
 	public void contextLoads() {
